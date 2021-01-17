4c4b3aede1cc5608fdc1c82252ea0b1383738a61
joshuiaclan <josh.aclan@gmail.com>
2021-01-18 07:39:05 +0800

    initialize commit

---
+++ hello-world.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int sum ( int x, int y ) {  return x + y; }
+int diff ( int x, int y ) {  return x - y; }
+int mult ( int x, int y ) {  return x * y; }
+int div ( int x, int y ) {  return x / y; }
+
+int main() {
+    int x, y, result = 0;
+    int operation;
+    printf("Enter 1st Integer: ");
+    scanf("%d", &x);
+    
+    printf("Enter 2nd Integer: ");
+    scanf("%d", &y);
+
+    printf("[1] Addition \n[2] Subtraction\n[3] Multiplication\n[4] Division\nChoose Operation:");
+    scanf("%d",  &operation);
+
+    switch(operation) {
+        case 1:
+            result = sum(x, y);
+            break;
+        case 2:
+            result = diff(x, y);
+            break;
+        case 3:
+            result = mult(x, y);
+            break;
+        case 4:
+            result = div(x, y);
+            break;
+    }
+
+    printf("Result = %d\n", result);
+
+    return 0;
+}
